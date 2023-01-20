af11c418b82f2929a2838e76f4035b23c0cf9228
ie017 <elorfissam1@gmail.com>
2023-01-20 23:34:11 +0100

    PathfindingAlgo

---
+++ src/Node.java
@@ -0,0 +1,17 @@
+
+public class Node {
+
+    int x;
+    int y;
+    double hValue;
+    int gValue;
+    double fValue;
+    Node parent;
+
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
